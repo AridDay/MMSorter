086eee74bb5729537c11bcbb5f4d56cb5937c102
AridDay <esakovpetr@gmail.com>
2018-03-20 19:19:50 -0400

    Added a Color Calibration Option

--- masterM_M/masterM_M.ino
+++ masterM_M/masterM_M.ino
@@ -1,8 +1,14 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
-
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X); //Color Sensor Integration && Gain settings
+int yellowArr[]={120,101,57}; //Magic numbers to compare colors to average calibrated color
+int blueArr[]={70,94,125};
+int greenArr[]={80,126,91};
+int redArr[]={172,80,80};
+int brownArr[]={118,96,78};
+int orangeArr[]={149,75,61};
 String readIn="";
+
 
 void setup() {
   pinMode(7,OUTPUT);
@@ -43,50 +49,250 @@ void morse(bool t)
   }
 }
 
-void getColor()
+struct Color{
+  int col[3];
+};
+
+struct Color getColor()
 {
+  struct Color ret;
+  uint16_t clr, red, green, blue;
+
+  digitalWrite(7,HIGH);
+
+  delay(60);  // takes 50ms to read 
+
   
-  uint16_t r, g, b, c, colorTemp, lux;
   
-  tcs.getRawData(&r, &g, &b, &c);
-  colorTemp = tcs.calculateColorTemperature(r, g, b);
-  lux = tcs.calculateLux(r, g, b);
+  tcs.getRawData(&red, &green, &blue, &clr);
+
+  digitalWrite(7,LOW);
   
-  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
+
+  // Conversion to hex code
+  uint32_t sum = clr;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+//  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
+//  Serial.println();
   
+//    Serial.print((int)r ); Serial.print(" "); Serial.print((int)g);Serial.print(" ");  Serial.println((int)b );
+    ret.col[0]=r;
+    ret.col[1]=g;
+    ret.col[2]=b;
+    return ret;
 }
 
-void loop() {
-  readIn=Serial.readString();
-
-//Console Tests
-  if(readIn=="reset"){
-      sendI2C(0xF);
-      Serial.println("Reset Imminent");
-      morse(true); morse(true); morse(true);      morse(false); morse(false); morse(false);     morse(true); morse(true); morse(true);
-      resetFunc();
-  }else if(readIn=="color")// Have to do 3 times, or else color isn't accurate. No clue why.
-  {
-    digitalWrite(7,HIGH);
-    getColor();
-    getColor();
-    getColor();
-    digitalWrite(7,LOW);
-  }
-  else if(readIn==""){}
+void findColor(int acc){
+//  Serial.println("Find Color");
+        struct Color in=getColor();
+      if(((brownArr[0]-acc)<in.col[0])&&(in.col[0]<(brownArr[0]+acc))
+       &&((brownArr[1]-acc)<in.col[1])&&(in.col[1]<(brownArr[1]+acc))
+       &&((brownArr[2]-acc)<in.col[2])&&(in.col[2]<(brownArr[2]+acc))
+      )
+      {
+        Serial.println("Brown");
+        sendI2C(0x4);
+        delay(100);
+      }else
+      if(((orangeArr[0]-acc)<in.col[0])&&(in.col[0]<(orangeArr[0]+acc))
+       &&((orangeArr[1]-acc)<in.col[1])&&(in.col[1]<(orangeArr[1]+acc))
+       &&((orangeArr[2]-acc)<in.col[2])&&(in.col[2]<(orangeArr[2]+acc))
+      ){
+        Serial.println("orange");
+        sendI2C(0x5);
+        delay(100);
+      }else
+      if(((greenArr[0]-acc)<in.col[0])&&(in.col[0]<(greenArr[0]+acc))
+       &&((greenArr[1]-acc)<in.col[1])&&(in.col[1]<(greenArr[1]+acc))
+       &&((greenArr[2]-acc)<in.col[2])&&(in.col[2]<(greenArr[2]+acc))
+      ){
+        Serial.println("green");
+        sendI2C(0x6);
+        delay(100);
+      }else
+      if(((redArr[0]-acc)<in.col[0])&&(in.col[0]<(redArr[0]+acc))
+       &&((redArr[1]-acc)<in.col[1])&&(in.col[1]<(redArr[1]+acc))
+       &&((redArr[2]-acc)<in.col[2])&&(in.col[2]<(redArr[2]+acc))
+      ){
+        Serial.println("red");
+        sendI2C(0x7);
+        delay(100);
+      }else
+      if(((yellowArr[0]-acc-5)<in.col[0])&&(in.col[0]<(yellowArr[0]+acc+5))
+       &&((yellowArr[1]-acc-5)<in.col[1])&&(in.col[1]<(yellowArr[1]+acc+5))
+       &&((yellowArr[2]-acc-5)<in.col[2])&&(in.col[2]<(yellowArr[2]+acc+5))
+      ){
+        Serial.println("yellow");
+        sendI2C(0x8);
+        delay(100);
+      }else
+      if(((blueArr[0]-acc)<in.col[0])&&(in.col[0]<(blueArr[0]+acc))
+       &&((blueArr[1]-acc)<in.col[1])&&(in.col[1]<(blueArr[1]+acc))
+       &&((blueArr[2]-acc)<in.col[2])&&(in.col[2]<(blueArr[2]+acc))
+      ){
+        Serial.println("blue");
+        sendI2C(0x9);
+        delay(100);
+      }
 }
 
-void sendI2C(byte b)
+ void sendI2C(byte b)
 {
   Wire.beginTransmission(2);
   Wire.write(b);
-  Wire.write(4);
   Wire.endTransmission();
 }
+
+struct Color sampleAverageColor()
+{
+  int rgb[9][3];
+  
+  for(int i = 0; i<9; i++) //Takes 9 Samples
+  {
+    struct Color in = getColor();
+    for(int j=0; j<3;j++)
+      rgb[i][j]=in.col[j];
+  }
+  
+  struct Color out;
+  
+  for(int i = 0; i<3;i++)
+  {
+    int av=0;
+    for(int j = 0; j<9;j++)
+      av+=rgb[j][i];
+
+    out.col[i]=av/9;
+  }
+  
+  return out;
+}
+
+
+void loop() {
+  findColor(5);
+
+    
+  
+  byte readIn=0x0;
+  
+
+
+  if (Serial.available() > 0) {
+       readIn = Serial.read();
+       Serial.println(readIn);
+  }
+//Console Tests
+//  if(readIn=="reset"){
+//      sendI2C(0xF);
+//      Serial.println("Reset Imminent");
+//      morse(true); morse(true); morse(true);      morse(false); morse(false); morse(false);     morse(true); morse(true); morse(true);
+//      resetFunc();
+//  }else if(readIn=="color")
+//  {
+//    for(int i=0; i<9;i++)
+//    {
+//    getColor();
+//    delay(200);
+//    }
+//    
+//
+//  }else if(readIn=="servom")
+//  {
+//    sendI2C(0x2);
+//    Serial.println("servom");
+//  }
+// 
+//  else if(readIn=="brn"){
+//    sendI2C(0x4);
+//    Serial.println("brn");
+//  }
+//  else if(readIn=="org"){
+//    sendI2C(0x5);
+//    Serial.println("org");
+//  }
+//  else if(readIn=="grn"){
+//    sendI2C(0x6);
+//    Serial.println("grn");
+//  }
+//  else if(readIn=="red"){
+//    sendI2C(0x7);
+//    Serial.println("red");
+//  }
+//  else if(readIn=="yel"){
+//    sendI2C(0x8);
+//    Serial.println("yel");
+//  }
+//  else if(readIn=="blu"){
+//    sendI2C(0x9);
+//    Serial.println("blu");
+//  }
+//  else 
+  
+  if(readIn==49){
+    
+    Serial.println("Color:");
+    Serial.flush();
+    while(!Serial.available());
+    String t=Serial.readString();
+    Serial.println(t);
+
+    struct Color in;
+    if(t=="yellow")
+    {
+      Serial.println("YellowCal");
+      in = sampleAverageColor();
+      for(int i = 0; i<3; i++)
+          yellowArr[i]=in.col[i];
+          
+    }else if(t=="blue")
+    {
+      Serial.println("BluCal");
+      in = sampleAverageColor();
+      for(int i = 0; i<3; i++)
+          blueArr[i]=in.col[i];
+
+    }else if(t=="green")
+    {
+      Serial.println("GRNCal");
+      in = sampleAverageColor();
+      for(int i = 0; i<3; i++)
+          greenArr[i]=in.col[i];
+    }
+    else if(t=="red")
+    {
+      Serial.println("RedCal");
+      in = sampleAverageColor();
+      for(int i = 0; i<3; i++)
+          redArr[i]=in.col[i];
+    }
+    else if(t=="brown")
+    {
+      Serial.println("BRNCal");
+      in = sampleAverageColor();
+      for(int i = 0; i<3; i++)
+          brownArr[i]=in.col[i];
+    }
+    else if(t=="orange")
+    {
+      Serial.println("ORGcal");
+      in = sampleAverageColor();
+      for(int i = 0; i<3; i++)
+          orangeArr[i]=in.col[i];
+    }
+   
+    
+    
+    t="";
+    Serial.println("calibrate");
+  }
+  
+  else if(readIn==0x0){}
+}
+
+
 
--- slaveM_M/slaveM_M.ino
+++ slaveM_M/slaveM_M.ino
@@ -4,10 +4,19 @@
 
 #define DEGREES_CONSTANT 366
 #define KILL_BYTE 0x00
+#define BROWN_POS 0 //1
+#define ORANGE_POS 60 //2
+#define GREEN_POS 120 //3
+#define RED_POS 180 //4
+#define YELLOW_POS 240 //5
+#define BLUE_POS 300 //6
+static volatile int stepperPosition;
+static volatile bool servoGoing=false;
 
 AF_Stepper motor(200, 1);
-Servo servo1;
+Servo servo1,servo2;
 String readIn="";
+int currentPos=0;
 
 void setup() {
   Serial.begin(9600);
@@ -15,50 +24,93 @@ void setup() {
   Wire.begin(2);
   Wire.onReceive(receiveEvent);
  
-  motor.setSpeed(10);  // 10 rpm   
+  motor.setSpeed(60);  //Stepper Speed 
   servo1.attach(9);
+  servo2.attach(10);
+  pinMode(2,INPUT);
 
   Serial.println("This is the M&M Slave");
 
 }
 
-void stepper(int deg,int mode)
+void pathfind(int pos)
 {
-  int s=(270*deg)/360;
+  if((pos-currentPos)<(360-pos+currentPos))
+  {
+    stepper((pos-currentPos),2);
+  }else
+  {
+    stepper(((360-pos+currentPos)*-1),2);
+  }
+}
+
+void stepper(double deg,int mode)
+{
+  Serial.print("DEG: ");
+  Serial.println(deg);
+  double s=(200.0/360.0)*deg;
+  Serial.print("S: ");
+  Serial.println(s);
+  Serial.print("CP: ");
+  Serial.println(currentPos);
+  if(deg>=0)
+  {
+    currentPos+=deg;
+  }else
+  {
+    currentPos= currentPos+deg;
+    Serial.print("CP2: ");
+    Serial.println(currentPos);
+  }
+  if(currentPos>=360)
+      currentPos= currentPos-360;
+  if(currentPos<0)
+      currentPos+=360;
+  Serial.print("CP3: ");
+  Serial.println(currentPos);
+ 
   switch(mode)
   {
     case 1:
       if(deg>0)
-        motor.step(s, FORWARD, SINGLE); 
-      else if(deg<0)
-        motor.step(s, BACKWARD, SINGLE);
+        motor.step((int)s, FORWARD, SINGLE); 
+      else{
+      s=abs(s);
+        motor.step((int)s, BACKWARD, SINGLE);}
     break;
     case 2:
       if(deg>0)
-        motor.step(s, FORWARD, DOUBLE); 
-      else if(deg<0)
-        motor.step(s, BACKWARD, DOUBLE);
+        motor.step((int)s, FORWARD, DOUBLE); 
+      else{
+      s=abs(s);
+        motor.step((int)s, BACKWARD, DOUBLE);}
     break;
     case 3:
       if(deg>0)
-        motor.step(s, FORWARD, INTERLEAVE); 
-      else if(deg<0)
-        motor.step(s, BACKWARD, INTERLEAVE);
+        motor.step((int)s, FORWARD, INTERLEAVE); 
+      else
+      {
+      s=abs(s);
+        motor.step((int)s, BACKWARD, INTERLEAVE);
+      }
     break;
     case 4:
       if(deg>0)
-        motor.step(s, FORWARD, MICROSTEP); 
-      else if(deg<0)
-        motor.step(s, BACKWARD, MICROSTEP);
+        motor.step((int)s, FORWARD, MICROSTEP); 
+      else
+      {
+         s=abs(s);
+        motor.step((int)s, BACKWARD, MICROSTEP);
+      }
     break;
   }
 }
 
 
 //True(Forward) False(Backward)
-void servo(int deg, bool direc) //Experimental and Highly Inaccurate!!!! ADD ROTARY ENCODER
+void servo(int deg, int direc) //Experimental and Highly Inaccurate!
 {
-  if(direc)
+  if(direc==1)
   {
     servo1.write(0);
     int t=(deg/90)*DEGREES_CONSTANT;
@@ -72,6 +124,19 @@ void servo(int deg, bool direc) //Experimental and Highly Inaccurate!!!! ADD ROT
     delay(t);
     servo1.write(90);
   }
+}
+
+
+void servoMag() /Accurate version of servo which uses Hall Effect Feedback
+{
+  servo1.write(0); Serial.println("Going");
+  while(digitalRead(2)==HIGH){}
+  servo1.write(90);Serial.println("Stopped");
+}
+
+void servoDeg(int deg)
+{
+  servo2.write(deg);
 }
 
 void(* resetFunc) (void) = 0; //Reset Everything Function
@@ -91,23 +156,134 @@ void receiveEvent(int howMany) {
     resetFunc();
     break;
     
-    case 0x1:
+    case 0x1: //Stepper
+
       t1=Wire.read();
+      Serial.println(t1);
+
       t2=Wire.read();
+      Serial.println(t2);
       stepper(t1,t2);
     break;
     
-    case 0x2:
-      t1=Wire.read();
-      t3=Wire.read();
-      servo(t1,t3);
+    case 0x2: //Servo 1 (cont)
+      servoGoing=true;
+      Serial.println("servom");
     break;
+
+    case 0x4:
+      stepperPosition=BROWN_POS;
+       Serial.println("brn");
+     break;
+    case 0x5:
+      stepperPosition=ORANGE_POS;
+      Serial.println("org");
+    break;
+    case 0x6:
+      stepperPosition=GREEN_POS;
+      Serial.println("grn");
+    break;
+    case 0x7:
+      stepperPosition=RED_POS;
+      Serial.println("red");
+    break;
+    case 0x8:
+      stepperPosition=YELLOW_POS;
+      Serial.println("yel");
+    break;
+    case 0x9:
+      stepperPosition=BLUE_POS;
+      Serial.println("blu");
+    break;
+
+     
   }
 }
 
 
-
 void loop() {
+  if(stepperPosition!=-1)
+  {
+    pathfind(stepperPosition);
+    stepperPosition=-1;
+  }
 
-  delay(100);
+  if(servoGoing)
+  {
+    servoMag();
+    servoGoing=false;
+  }
+
+readIn=Serial.readString();
+ if(readIn=="stepper"){
+    Serial.println("Stepper");
+    String t="";
+    int t1;
+    int t2;
+    
+    Serial.println("Degrees");
+    Serial.flush();
+    while(!Serial.available());
+    t=Serial.readString();
+    t1=t.toInt();
+    Serial.println(t1);
+    t="";
+
+    Serial.println("Mode");
+    Serial.flush();
+    while(!Serial.available());
+    t=Serial.readString();
+    t2=t.toInt();
+    Serial.println(t2);
+    t="";
+
+    stepper(t1,t2);
+    
+  }
+  else if(readIn=="servo1"){
+    Serial.println("Continuous Servo");
+    String t;
+    int t1;
+    int t2;
+    
+    Serial.println("Degrees:");
+    Serial.flush();
+    while(!Serial.available());
+    t=Serial.readString();
+    t1=t.toInt();
+    Serial.println(t1);
+    t="";
+    
+    
+    Serial.println("Direction:");
+    Serial.flush();
+    while(!Serial.available());
+    t=Serial.readString();
+    t2=t.toInt();
+    Serial.println(t2);
+    t="";
+
+    servo(t1,t2);
+  }
+  else if(readIn=="servom"){
+    Serial.println("Continuous Servo Mag");
+    servoMag();
+  }
+  else if(readIn=="path"){
+    Serial.println("Path");
+    String t;
+    int t1;
+    
+    Serial.println("Pos:");
+    Serial.flush();
+    while(!Serial.available());
+    t=Serial.readString();
+    t1=t.toInt();
+    Serial.println(t1);
+    t="";
+    
+
+    pathfind(t1);
+  }
+  
 }
